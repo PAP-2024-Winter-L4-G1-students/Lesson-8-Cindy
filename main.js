9afd17442dba886ab5080652555ba737300630d0
cindyn04 <cindyn037@gmail.com>
2024-01-11 18:36:14 -0800

    just finished assignment 8

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 //Exercise 1 - Create an array of your 6 favorite things (or more)
 
+let favItems = ['Food', 'Dogs', 'Energy Drinks', 'Sleep', 'Plants'];
 
+let secondFav = favItems[1];
+console.log("My second favorite thing is " + secondFav);
 //Exercise 2 - Access your most favorite thing from the array and show it on the page
 
+let myFavlist = document.getElementById('my-favorite');
+myFavlist.innerHTML = `<li>${secondFav}</li>`;
 
 //Exercise 3 - change your favorite thing and show it on the page
 
+myFavlist[3] = "Traveling";
+myFavlist.innerHTML = `<li>${myFavlist[3]}</li>`;
+myFavlist.innerHTML = `<li>${myFavlist[4]}</li>`;
 
 //Exercise 4 - Use the length property to show the number of items in your array
 
-
+let total = document.getElementById('total');
+total.textContent = favItems.length;
 
 /*Exercise 5 - Iterate through the array and show it on the page using:
  1. A for loop with the of keyword
  2. Edit the for loop to use the in keyword to show the index numbers
  */
 
+let allFavorites = document.getElementById('all-favorites');
+for (let favorite of favItems) {
+    allFavorites.innerHTML += `<li>${favorite}</li>`;
+}
 
 
 /*Exercise 6 - Iterate through the array and show it on the page
